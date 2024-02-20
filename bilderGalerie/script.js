4a96f6f479e78043579bd8705ab0b209decf3563
DurnaMa <durna.m@outlook.de>
2024-02-20 23:37:38 +0100

    add image button

---
+++ bilderGalerie/script.js
@@ -0,0 +1,15 @@
+function image1() {
+    document.getElementById('image').src = '../bilderGalerie/image/iceland.jpg';
+}
+
+function image2() {
+    document.getElementById('image').src = '../bilderGalerie/image/volcano1.jpg';
+}
+
+function image3() {
+    document.getElementById('image').src = '../bilderGalerie/image/mount-fuji.jpg';
+}
+
+function image4() {
+    document.getElementById('image').src = '../bilderGalerie/image/volcano2.jpg';
+}
