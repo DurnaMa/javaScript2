beca3c47b78b80f8ae553fa57a055a1cb3bee013
DurnaMa <durna.m@outlook.de>
2024-02-21 23:15:52 +0100

    add adventsKalender

--- forSchleife/script.js
+++ forSchleife/script.js
@@ -8,7 +8,7 @@ let images = [
 let currentImage = 0;
 
 function inserDoors() {
-    for (let i = 0; i < 24; i++) {
-        document.getElementById("body").innerHTML += `<div class="door"></div>`;
+    for (let i = 1; i < 25; i++) {
+        document.getElementById("body").innerHTML += `<div class="door">${i}</div>`;
     }
 }
