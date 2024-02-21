0304262b27ae916c61c0c9be58d29fd129857edd
DurnaMa <durna.m@outlook.de>
2024-02-21 13:43:49 +0100

    add arrays und i++

--- arrays und i++/script.js
+++ arrays und i++/script.js
@@ -7,25 +7,25 @@ let images = [
 let i = 0;
 
 function image1() {
-    document.getElementById("image").src = "../image/iceland.jpg";
+    document.getElementById("image").src = images[i]; // I hat den wrt 0
     i++;
     setTimeout(image2, 3000);
 }
 
 function image2() {
-    document.getElementById("image").src = "../image//volcano1.jpg";
+    document.getElementById("image").src = images[i]; // I hat den wrt 1
     i++;
     setTimeout(image3, 3000);
 }
 
 function image3() {
-    document.getElementById("image").src = "../image//mount-fuji.jpg";
+    document.getElementById("image").src = images[i]; // I hat den wrt 2
     i++;
     setTimeout(image4, 3000);
 }
 
 function image4() {
-    document.getElementById("image").src = "../image/image/volcano2.jpg";
+    document.getElementById("image").src = images[i]; // I hat den wrt 4
     i = 0
     setTimeout(image1, 3000);
 }
